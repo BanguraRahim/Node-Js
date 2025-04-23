aae9195dcc4469f5af41cfcd72da14ee747f23e0
“BanguraRahim” <ibrahimsoriebangura218@gmail.com>
2025-04-23 08:03:25 +0000

    FIle handling using callbacks

--- 01tut/server.js
+++ 01tut/server.js
@@ -1,21 +1,23 @@
 console.log("Hello world")
-//console.log(global)
+/*
+console.log(global)
 //common js import
-// const os = require('os')
-// const path = require('path')
+const os = require('os')
+const path = require('path')
 
-// console.log(os.type())
-// console.log(os.version())
-// console.log(os.homedir())
+console.log(os.type())
+console.log(os.version())
+console.log(os.homedir())
 
-// console.log(__dirname)
-// console.log(__filename)
+console.log(__dirname)
+console.log(__filename)
 
-// console.log(path.dirname(__filename))
-// console.log(path.basename(__filename))
-// console.log(path.extname(__filename))
+console.log(path.dirname(__filename))
+console.log(path.basename(__filename))
+console.log(path.extname(__filename))
 
-// console.log(path.parse(__filename))
+console.log(path.parse(__filename))
+*/
 
 const {add,subtract,multiply,divide} = require('./math')
 console.log(add(3,2))
--- 02tut/index.js
+++ 02tut/index.js
@@ -6,23 +6,24 @@ fs.readFile(path.join(__dirname, 'files','starter.txt'),'utf8', (err, data) => {
   console.log(data);
 })
 
-console.log('Hello....');
+
 
 fs.writeFile(path.join(__dirname, 'files','reply.txt'), 'Nice to meet you.', (err) => {
   if (err) throw err;
   console.log('Write complete');
 
-  fs.appendFile(path.join(__dirname, 'files','reply.txt'), '\n\nYes it is.', (err) => {
-    if (err) throw err;
-    console.log('Append complete');
-    
-    fs.rename(path.join(__dirname, 'files','reply.txt'), path.join(__dirname, 'files','newReply.txt'), (err) => {
+    fs.appendFile(path.join(__dirname, 'files','reply.txt'), '\n\nYes it is.', (err) => {
       if (err) throw err;
-      console.log('Rename complete');
+      console.log('Append complete');
+    
+        fs.rename(path.join(__dirname, 'files','reply.txt'), path.join(__dirname, 'files','newReply.txt'), (err) => {
+          if (err) throw err;
+          console.log('Rename complete');
  
     })
   })
 })
+
 
 
 
