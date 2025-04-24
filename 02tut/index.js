cfb76311910c65d53b42daa1b1924b73ed101924
“BanguraRahim” <ibrahimsoriebangura218@gmail.com>
2025-04-24 15:56:53 +0000

    file handling using promises

--- 02tut/index.js
+++ 02tut/index.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
+const fsPromises = require('fs').promises;
+const { writeFile } = require('fs');
 const path = require('path')
-
+/*
 fs.readFile(path.join(__dirname, 'files','starter.txt'),'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
@@ -33,3 +34,21 @@ process.on('uncaughtException', err => {
   process.exit(1);
 })
 
+*/
+
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(path.join(__dirname, 'files','starter.txt'),'utf8');
+    await fsPromises.unlink(path.join(__dirname, 'files','starter.txt'));
+    console.log(data);
+    await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data);
+    await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\n\nNice to meet you');
+    await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'), path.join(__dirname, 'files', 'promiseComplete.txt'));
+    const newData = await fsPromises.readFile(path.join(__dirname, 'files','promiseComplete.txt'),'utf8');
+    console.log(newData);
+  } catch (err) {
+    console.error(`There was an uncaught error: ${err}`);
+  }
+}
+
+fileOps();
