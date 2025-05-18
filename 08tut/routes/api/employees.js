a9f894db9fea950a2ba22ecc598ebcf3556a22a8
“BanguraRahim” <ibrahimsoriebangura218@gmail.com>
2025-05-18 08:14:05 +0000

    made small changes on chapter 8 and continued with chapter 9

--- 08tut/routes/api/employees.js
+++ 08tut/routes/api/employees.js
@@ -19,7 +19,7 @@ router.route('/')
             res.status(400).send("Inputs not found")
         }
         else {
-            const id = data.employees.length ? data.employees.length + 1 : 1;
+            const id = data.employees.length ? data.employees[data.employees.length - 1].id + 1 : 1;
             const newEmployee = {
                 id,
                 "firstname": req.body.firstname,
@@ -28,7 +28,7 @@ router.route('/')
             const allEmployees = [
                 ...data.employees, newEmployee
             ]
-            data.employees = allEmployees;
+            data.employees = allEmployees.sort((a,b) => a.id - b.id);
             res.status(201).json({
                 message: "New employee created successfully",
                 employee: newEmployee
@@ -36,30 +36,42 @@ router.route('/')
         }
     })
     .put((req, res) => {
-       const employee = data.employees.find(emp => emp.id === req.body.id);
+    
+        const { id, firstname, lastname} = req.body;
+        if(!id || !firstname || !lastname) {
+            res.status(400).json({ error: "ID, firstname, and lastname are required." });
+        }
 
-       if(!employee) {
-        res.status(404).json({error: "Employee not found"});
-       }
+        const employee = data.employees.find(emp => emp.id === parseInt(id));
 
-       // Update the found employee
+        if(!employee) {
+            return res.status(404).json({error: "Employee not found"});
+        }
+
+        // Update the found employee
         employee.firstname = req.body.firstname;
         employee.lastname = req.body.lastname;
 
-        res.json(employee);
-    
+        //Replace old employee with new employee
+        const updatedEmployee = data.employees.map(emp => 
+            emp.id === parseInt(id) ? employee : emp
+        );
+        updatedEmployee.sort((a,b) => a.id - b.id);
+        data.employees = updatedEmployee;
+        res.json(updatedEmployee);
         
     })
     .delete((req, res) => {
-            const findEmployee = data.employees.find(emp => emp.id === req.body.id);
+        const findEmployee = data.employees.find(emp => emp.id === Number(req.body.id));
 
-            if (findEmployee) {
-                const employee = data.employees.filter(emp => emp.id !== req.body.id);
-                return res.json(employee);
-            }
-            else {
-                res.status(404).json({error: "Employee not found"});
-            }  
+        if (findEmployee) {
+            const employee = data.employees.filter(emp => emp.id !== Number(req.body.id));
+            data.employees = employee;
+            res.json(data.employees);
+        }
+        else {
+            res.status(404).json({error: "Employee not found"});
+        }  
     });
 
     router.route('/:id')
